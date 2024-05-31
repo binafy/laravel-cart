b8975042b26fb168b8bf575315899b2031b57034
Milwad <milwad.dev@gmail.com>
2024-05-31 13:58:30 +0330

    Create 2024_05_31_102710_create_carts_table.php

---
+++ database/migrations/2024_05_31_102710_create_carts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $tableName = config('laravel-cart.users.table', 'users');
+
+        Schema::create('carts', function (Blueprint $table) use ($tableName) {
+            $table->id();
+
+            $table->foreignId('user_id')->constrained($tableName)->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carts');
+    }
+};
