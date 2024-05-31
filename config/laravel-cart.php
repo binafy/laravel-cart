c8cb7482c971d12a6798198ec4b41dbadddc1803
Milwad <milwad.dev@gmail.com>
2024-05-31 19:50:49 +0330

    Update laravel-cart.php

--- config/laravel-cart.php
+++ config/laravel-cart.php
@@ -1,17 +1,49 @@
 <?php
 
 return [
+    /*
+     * Users
+     *
+     * This value is about the users table, foreign key and, ... .
+     */
     'users' => [
+        /*
+         * The user table name.
+         */
         'table' => 'users',
+
+        /*
+         * The user foreign key.
+         */
         'foreign_id' => 'user_id',
     ],
 
+    /*
+     * Carts
+     *
+     * This value is about the cart table name, foreign key and, ... .
+     */
     'carts' => [
+        /*
+         * The cart table name.
+         */
         'table' => 'carts',
+
+        /*
+         * The cart foreign key name.
+         */
         'foreign_id' => 'cart_id',
     ],
 
+    /*
+     * Cart Items
+     *
+     * This value is about the items of one cart.
+     */
     'cart_items' => [
+        /*
+         * The cart items table name.
+         */
         'table' => 'cart_items',
     ],
 ];
