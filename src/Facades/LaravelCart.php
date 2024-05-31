8d70f3e0a7cfb1ba731f74922f3ab56ddd7306ff
Milwad <milwad.dev@gmail.com>
2024-05-31 14:22:38 +0330

    Update LaravelCart.php

--- src/Facades/LaravelCart.php
+++ src/Facades/LaravelCart.php
@@ -11,6 +11,6 @@ class LaravelCart extends Facade
      */
     protected static function getFacadeAccessor(): string
     {
-        return LaravelCart::class;
+        return \Binafy\LaravelCart\LaravelCart::class;
     }
 }
