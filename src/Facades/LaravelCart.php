80c39092635640b6ea937610b88cb39d56ec633b
Milwad <milwad.dev@gmail.com>
2024-05-31 14:22:11 +0330

    Create LaravelCart.php

---
+++ src/Facades/LaravelCart.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Binafy\LaravelCart\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class LaravelCart extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return LaravelCart::class;
+    }
+}
