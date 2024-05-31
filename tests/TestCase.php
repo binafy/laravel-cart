87a2a8d8c48af534cc49e37c1a131b1af4a8be5c
Milwad <milwad.dev@gmail.com>
2024-05-31 14:16:50 +0330

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,40 @@
 
 namespace Tests;
 
-use PHPUnit\Framework\TestCase as BaseTestCase;
+use Illuminate\Encryption\Encrypter;
 
-abstract class TestCase extends BaseTestCase
+abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
-    //
+    /**
+     * Load package service provider.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [LaravelCartServiceProvider::class];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Set default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testing');
+        $app['config']->set('database.connections.testing', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+
+        // Set app key
+        $app['config']->set('app.key', 'base64:'.base64_encode(
+            Encrypter::generateKey(config()['app.cipher'])
+        ));
+    }
 }
