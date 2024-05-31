c957964367b5599aa8dff9c6c3686e6f5e9874e3
Milwad <milwad.dev@gmail.com>
2024-05-31 14:06:39 +0330

    Create CartItem.php

---
+++ src/Models/CartItem.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CartItem extends Model
+{
+    use HasFactory;
+
+    /**
+     * Fillable columns.
+     *
+     * @var string[]
+     */
+    protected $fillable = ['cart_id', 'itemable_id', 'itemable_type', 'quantity'];
+
+    /**
+     * Relation polymorphic, inverse one-to-one or many relationship.
+     */
+    public function itemable(): \Illuminate\Database\Eloquent\Relations\MorphTo
+    {
+        return $this->morphTo();
+    }
+}
