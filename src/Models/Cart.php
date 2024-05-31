0804ba657bf70edcfc6a75cfdb6c85a16e3e6a9b
Milwad <milwad.dev@gmail.com>
2024-05-31 19:43:39 +0330

    wip

--- src/Models/Cart.php
+++ src/Models/Cart.php
@@ -14,6 +14,16 @@ class Cart extends Model
     protected $fillable = ['user_id'];
 
     /**
+     * Create a new instance of the model.
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->table = config('laravel-cart.carts.table', 'carts');
+    }
+
+    /**
      * Relation one-to-many, CartItem model.
      */
     public function items(): \Illuminate\Database\Eloquent\Relations\HasMany
--- src/Models/CartItem.php
+++ src/Models/CartItem.php
@@ -2,19 +2,26 @@
 
 namespace Binafy\LaravelCart\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CartItem extends Model
 {
-    use HasFactory;
-
     /**
      * Fillable columns.
      *
      * @var string[]
      */
     protected $fillable = ['cart_id', 'itemable_id', 'itemable_type', 'quantity'];
+
+    /**
+     * Create a new instance of the model.
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->table = config('laravel-cart.cart_items.table', 'cart_items');
+    }
 
     /**
      * Relation polymorphic, inverse one-to-one or many relationship.
