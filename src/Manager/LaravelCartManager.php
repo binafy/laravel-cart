973e11f8bef0c0101b7543b7842c98efb16b2918
Milwad Khosravi <milwad.dev@gmail.com>
2024-07-07 11:09:43 +0330

    Update LaravelCartManager.php

--- src/Manager/LaravelCartManager.php
+++ src/Manager/LaravelCartManager.php
@@ -2,7 +2,23 @@
 
 namespace Binafy\LaravelCart\Manager;
 
-class LaravelCartManager
-{
+use Illuminate\Support\Manager;
 
+class LaravelCartManager extends Manager
+{
+    /**
+     * Get the default driver.
+     */
+    public function getDefaultDriver(): string
+    {
+        return 'database';
+    }
+
+    /**
+     * The database driver of laravel cart.
+     */
+    public function createDatabaseDriver(): LaravelCartDatabase
+    {
+        return new LaravelCartDatabase();
+    }
 }
