4b3bb9087c22589ea5fe488f23d3a29bb3e46de9
Milwad Khosravi <milwad.dev@gmail.com>
2024-07-07 11:18:08 +0330

    Update LaravelCartDatabase.php

--- src/Drivers/LaravelCartDatabase.php
+++ src/Drivers/LaravelCartDatabase.php
@@ -2,7 +2,36 @@
 
 namespace Binafy\LaravelCart\Drivers;
 
-class LaravelCartDatabase
+class LaravelCartDatabase implements Driver
 {
 
+    public function storeItem()
+    {
+        // TODO: Implement storeItem() method.
+    }
+
+    public function storeItems()
+    {
+        // TODO: Implement storeItems() method.
+    }
+
+    public function increaseQuantity()
+    {
+        // TODO: Implement increaseQuantity() method.
+    }
+
+    public function decreaseQuantity()
+    {
+        // TODO: Implement decreaseQuantity() method.
+    }
+
+    public function removeItem()
+    {
+        // TODO: Implement removeItem() method.
+    }
+
+    public function empty()
+    {
+        // TODO: Implement empty() method.
+    }
 }
