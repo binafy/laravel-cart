963dcd8d07410a78129129935e4469345429d603
Milwad Khosravi <milwad.dev@gmail.com>
2024-07-21 12:29:08 +0330

    add `options` column to cart_items table

--- database/migrations/2024_05_31_103315_create_cart_items_table.php
+++ database/migrations/2024_05_31_103315_create_cart_items_table.php
@@ -21,7 +21,8 @@ return new class extends Migration
             $table->foreignId($cartForeignName)->constrained($cartTableName)->cascadeOnDelete();
             $table->morphs('itemable');
             $table->unsignedInteger('quantity')->default(1);
-
+            $table->json('options')->nullable();
+            
             $table->timestamps();
         });
     }
