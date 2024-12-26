71c6dcbf370b5f188b1418d1a92c98861e941db6
Milwad <98118400+milwad-dev@users.noreply.github.com>
2024-12-26 16:20:12 +0330

    Update LaravelCart.php

--- src/LaravelCart.php
+++ src/LaravelCart.php
@@ -2,8 +2,6 @@
 
 namespace Binafy\LaravelCart;
 
-use Illuminate\Support\Facades\Facade;
-
 /**
  * @method static \Binafy\LaravelCart\Drivers\Driver driver(string|null $driver = null)
  * @method static \Binafy\LaravelCart\Drivers\Driver storeItem(\Illuminate\Database\Eloquent\Model|array $item, int|null $userId = null)
@@ -12,12 +10,16 @@ use Illuminate\Support\Facades\Facade;
  * @method static \Binafy\LaravelCart\Drivers\Driver decreaseQuantity(\Illuminate\Database\Eloquent\Model $item, int $quantity = 1)
  * @method static \Binafy\LaravelCart\Drivers\Driver removeItem(\Illuminate\Database\Eloquent\Model $item)
  * @method static \Binafy\LaravelCart\Drivers\Driver emptyCart()
+ * @method static \Binafy\LaravelCart\Drivers\Driver getOption(string $option, ?int $itemId = null, ?int $userId = null)
+ * @method static \Binafy\LaravelCart\Drivers\Driver getOptions(?int $itemId = null, ?int $userId = null)
+ * @method static \Binafy\LaravelCart\Drivers\Driver setOption(string $key, mixed $value, ?int $itemId = null, ?int $userId = null)
+ * @method static \Binafy\LaravelCart\Drivers\Driver addOption(string $key, mixed $value, ?int $itemId = null, ?int $userId = null)
  * @method static string getDefaultDriver()
  * @method static void setDefaultDriver(string $name)
  *
  * @see \Binafy\LaravelCart\Manager\LaravelCartManager
  */
-class LaravelCart extends Facade
+class LaravelCart extends \Illuminate\Support\Facades\Facade
 {
     /**
      * Get the registered name of the component.
