9c0591def1dec26ae3e6fc0647df6473d1a23ade
Matthew Forth <matty@studiorepublic.com>
2025-10-09 08:55:46 +0100

    fix: updated drivers to use string userId

--- src/Drivers/Driver.php
+++ src/Drivers/Driver.php
@@ -6,15 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 interface Driver
 {
-    public function storeItem(Model|array $item, ?int $userId = null): Driver;
+    public function storeItem(Model|array $item, ?string $userId = null): Driver;
 
-    public function storeItems(array $items, ?int $userId = null): Driver;
+    public function storeItems(array $items, ?string $userId = null): Driver;
 
-    public function increaseQuantity(Model $item, int $quantity = 1, ?int $userId = null): Driver;
+    public function increaseQuantity(Model $item, int $quantity = 1, ?string $userId = null): Driver;
 
-    public function decreaseQuantity(Model $item, int $quantity = 1, ?int $userId = null): Driver;
+    public function decreaseQuantity(Model $item, int $quantity = 1, ?string $userId = null): Driver;
 
-    public function removeItem(Model $item, ?int $userId = null): Driver;
+    public function removeItem(Model $item, ?string $userId = null): Driver;
 
-    public function emptyCart(?int $userId = null): Driver;
+    public function emptyCart(?string $userId = null): Driver;
 }
